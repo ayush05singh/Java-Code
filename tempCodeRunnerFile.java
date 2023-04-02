aeb66761986f822077949fdd9503d9a35188bdd0
ayush05singh <ayushkumar2205@gmail.com>
2023-04-03 02:05:16 +0530

    Today Work

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,2 +1,26 @@
- // var d=sc.nextLine();
-        // System.out.println(d.matches("^\\d{2}/\\d{2}/\\d{4}$"));
+  // int mar1, mar2, mar3 ;
+        // double percent;
+        // System.out.print("Enter mar1 = ");
+        // mar1=sc.nextInt();
+        // System.out.print("Enter mar2 = ");
+        // mar2=sc.nextInt();
+        // System.out.print("Enter mar3 = ");
+        // mar3=sc.nextInt();
+
+        // percent = ((mar1+mar2+mar3)/300.0)*100;
+        // if (percent<=100 && percent>90)
+        // {
+        //     System.out.println("Congrats! You got 'A' grade with "+percent+"%");
+        // }
+        // else if (percent<=90 && percent>=80)
+        // {
+        //     System.out.println("Congrats! You got 'B' grade with "+percent+"%");
+        // }
+        // else if(percent<=80 && percent>=70)
+        // {
+        //     System.out.println("Congrats! You got 'c' grade with "+percent+"%");
+        // }
+        // else
+        // {
+        //     System.out.println("You got less grade with "+percent+"%"+" need improvment.");
+        // }
