b6441af93d5aed7d9f9c295b8019886b3424c195
ayush05singh <ayushkumar2205@gmail.com>
2023-04-08 12:54:55 +0530

    08-04-23

--- loopques3.java
+++ loopques3.java
@@ -8,56 +8,65 @@ public class loopques3
     {
         Scanner sc=new Scanner(System.in);
         
-        System.out.print("Enter a Number = ");
-        String str=sc.nextLine();
+        // System.out.print("Enter a Number = ");
+        // String str=sc.nextLine();
         
-        char c;
-        for(int i=0;i<str.length();i++)
-        {
-            c=str.charAt(i);
-            switch(c)
-            {
-                case '0':System.out.print("Zero ");
-                        break;
-                case '1':System.out.print("One ");
-                        break;
-                case '2':System.out.print("Two ");
-                        break;
-                case '3':System.out.print("Three ");
-                        break;
-                case '4':System.out.print("Four ");
-                        break;
-                case '5':System.out.print("Five ");
-                        break;
-                case '6':System.out.print("Six ");
-                        break;
-                case '7':System.out.print("Seven ");
-                        break;
-                case '8':System.out.print("Eight ");
-                        break;
-                case '9':System.out.print("Nine ");
-                        break;
+        // char c;
+        // for(int i=0;i<str.length();i++)
+        // {
+        //     c=str.charAt(i);
+        //     switch(c)
+        //     {
+        //         case '0':System.out.print("Zero ");
+        //                 break;
+        //         case '1':System.out.print("One ");
+        //                 break;
+        //         case '2':System.out.print("Two ");
+        //                 break;
+        //         case '3':System.out.print("Three ");
+        //                 break;
+        //         case '4':System.out.print("Four ");
+        //                 break;
+        //         case '5':System.out.print("Five ");
+        //                 break;
+        //         case '6':System.out.print("Six ");
+        //                 break;
+        //         case '7':System.out.print("Seven ");
+        //                 break;
+        //         case '8':System.out.print("Eight ");
+        //                 break;
+        //         case '9':System.out.print("Nine ");
+        //                 break;
                        
-            }
+        //     }
+        // }
    // ----------------------------------------------------------------------------------------------------------- //
    
-//         System.out.println("Enter value of 'a' = ");
-//         int a = sc.nextInt();
-//         System.out.println("Enter value of 'd' = ");
-//         int d = sc.nextInt();
-//         System.out.println("Enter value of 'n' = ");
-//         int n = sc.nextInt();
+        System.out.print("Enter value of 'a' = ");
+        int a = sc.nextInt();
+        System.out.print("Enter value of 'd' = ");
+        int d = sc.nextInt();
+        System.out.print("Enter value of 'n' = ");
+        int n = sc.nextInt();
+        String str="";
+        
+        // for A.P. given below:-
+        // for (int i=0;i<n;i++)
+        // {       
+        //         System.out.print(a+",");
+        //         a = a +d;
+        //         str = str+a; 
+        // }
+        // System.out.println();
+        // System.out.print(str+",");
 
-//         for ()
-
-
-
-
-
-            sc.close();
-
+        // for G.P. given below:-
+        for (int i=0;i<n;i++)
+        {
+                System.out.print(a+",");
+                a=a*d;
         }
-    }
-    
-}
 
+        sc.close();
+    }
+}    
